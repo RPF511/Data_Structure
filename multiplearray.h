b06d9b64d62812e8b26401eb80f7145513a46b40
RPF511 <sbk98726@gmail.com>
2019-10-19 20:05:02 +0900

    allocate multiple array dynamically

---
+++ multiplearray.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int** mk_2dim_array(int m, int n);
+
+void delete_2dim_array(int **arr);
+
+void print_2dim_array(int **arr, int m, int n);
+
+void scanf_2dim_array(int **arr,int m, int n);
+
+void copy_2dim_array(int **arr, int **from,int m, int n);
+
+void input_2dim_array(int **arr, int *from, int size);
+
+void print_array(int *arr,int size);
